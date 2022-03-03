d143ddc06be837c68c3a439916d2f90bbbde85e0
ishfaq <you@example.com>
2022-03-03 19:49:21 +0500

    INLINE CRUD IN REACT JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Home'
+import {About}  from './About'
+import {BrowserRouter as Router , Link,Route,Switch,Redirect} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+       <Link to="/">Home</Link>
+       <Link to="/About">About</Link>
+       <Link 
+    to={{ 
+    pathname: '/About', 
+    state: { message: 'hello, im a passed message!' } 
+  }}/>
+       <Switch>
+         <Route exact path="/" component={Home}/>
+         <Route exact path="/About" component={About}/>
+       </Switch>
+       <Redirect to="/"/>
+     </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Users } from './Users';
+import { InlineEdit } from './InlineEdit';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InlineEdit />
   </React.StrictMode>,
   document.getElementById('root')
 );
